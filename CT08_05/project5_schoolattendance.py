6d14c1d4b56447c0798ade13410fc7f8f58f0180
WarrenAndTheTwins <warrenasks@gmail.com>
2026-03-01 04:24:56 +0000

    updates

--- CT08_05/project5_schoolattendance.py
+++ CT08_05/project5_schoolattendance.py
@@ -8,36 +8,75 @@
 # - (True for present, False for absent).​
 # - This dictionary will serve as the database for all subsequent tasks.
 
-attendance_dict = {
-    "john": [True, False, True],
-    "jane": [True, True, True],
-    "bob": [False, False, True],
-    "alice": [True, True, False],
-    "sam": [True, True, True],
-    "eve": [False, True, False],
-    "tom": [True, False, False],
-    "daisy": [True, True, True],
-    "mike": [False, False, False],
-    "lily": [True, False, True]
-}
+# attendance_dict = {
+#     "john": [True, False, True],
+#     "jane": [True, True, True],
+#     "bob": [False, False, True],
+#     "alice": [True, True, False],
+#     "sam": [True, True, True],
+#     "eve": [False, True, False],
+#     "tom": [True, False, False],
+#     "daisy": [True, True, True],
+#     "mike": [False, False, False],
+#     "lily": [True, False, True]
+# }
+
+# def take_attendance(attendance_dict):
+#     for student in attendance_dict:
+#         ans = input(f"Is {student} present? (y/n): ")
+#         while ans != "y" and ans != "n":
+#             ans = input("Invalid input. Please enter 'y' or 'n': ")
+#         if ans == 'y':
+#             attendance_dict[student].append(True)
+#         if ans == 'n':
+#             attendance_dict[student].append(False)
+
+#     return attendance_dict
+# def calculate_attendance_percentage(student_name, attendance_dict):
+#     student_attendance = attendance_dict[student_name]
+#     total_days = len(student_attendance)
+#     present_days = sum(student_attendance)
+#     percentage = (present_days / total_days * 100) if total_days > 0 else 0
+#     return percentage
+
+# def notify_low_percentage(attendance_dict, threshold):
+#     low_attendance = []
+#     for name in attendance_dict:
+#         student_percentage = calculate_attendance_percentage(name, attendance_dict)
+#         if student_percentage < threshold:
+#             low_attendance.append(name)
+#     return low_attendance
+
+# def menu():
+
+#     while True:
+#         print("School Attendance System")
+#         print("1: view attendance")
+#         print("2: take attendance")
+#         print("3: Given a student 𝘯, find the attendance percentage of that student. Enter in lowest case.")
+#         print("4: look at low attendance students")
+#         selection = input("Enter selection :)")
+#         if int(selection) == 1:
+#             print(attendance_dict)
+#         elif int(selection) == 2:
+#             take_attendance(attendance_dict)
+#         elif int(selection) == 3:
+#             student_picked = input("Which student?")
+#             if student_picked in attendance_dict:
+#                 that_attendance = calculate_attendance_percentage(student_picked, attendance_dict)
+#                 print(that_attendance)
+#             else:
+#                 print("nuh uh")
+#         elif int(selection) == 4:
+#             print(notify_low_percentage(attendance_dict, 50))
+#         else:
+#             break
 
 
-def take_attendance(attendance_dict):
-    for student in attendance_dict:
-        ans = input(f"Is {student} present? (y/n): ")
-        while ans != "y" and ans != "n":
-            ans = input("Invalid input. Please enter 'y' or 'n': ")
-        if ans == 'y':
-            attendance_dict[student].append(True)
-        if ans == 'n':
-            attendance_dict[student].append(False)
+# menu()
 
-    return attendance_dict
-def calculate_attendance_percentage(student_name, attendance_dict):
-    student_attendance = attendance_dict[student_name]
-    total_days = len(student_attendance)
-    present_days = sum(student_attendance)
-    percentage = (present_days / total_days * 100) if total_days > 0 else 0
-    return percentage
-
-take_attendance(attendance_dict)
+# jonny : [True, True, True]
+# sally : [False, False, True]
+# becky : [False, True, True]
+# mark : [True, True, True]
+# linda : [False, False, False]
---
+++ CT08_06/lesson6.py
@@ -0,0 +1,43 @@
+answer_key = ["A", "B", "B", "D"]  
+student_answers = {
+    "john": ["A", "C", "B", "D"],
+    "jane": ["A", "B", "B", "D"],
+    "alice": ["A", "C", "C", "D"],
+    "bob": ["A", "B", "B", "D"]
+}
+
+def grade_all_students(student_answers, answer_key):
+    quiz_scores = {}
+    for name, answers in student_answers.items():
+        temp_score = 0
+        for i in range(len(answer_key)):
+            if answers[i] == answer_key[i]:
+                temp_score += 1
+        quiz_scores[name] = temp_score/len(answer_key) * 100
+    return quiz_scores
+quiz_scores = grade_all_students(student_answers, answer_key)
+print(quiz_scores)
+
+def calculate_average_score(quiz_scores):
+    sum = 0
+    for _, score in quiz_scores.items():
+        sum += score
+    return sum/len(quiz_scores)
+def find_highest_scorer(quiz_scores):
+    highest_scorers = []
+    max_score = 0
+    for _, score in quiz_scores.items():
+        if score > max_score:
+            max_score = score
+    for name, score in quiz_scores.items():
+        if score == max_score:
+           highest_scorers.append(name)
+    return highest_scorers
+def display_results(quiz_scores):
+    for name, score in quiz_scores:
+        print(f"name", "score")
+
+
+    
+
+
